bc1377440699ad93dce5a3a94644107dee95ef11
SantiagoHuh <ayrancan619@gmail.com>
2019-07-05 14:45:56 -0500

    Cambio menor
    Cambio de la bd

--- vistas/modulos/inicio/productos-recientes.php
+++ vistas/modulos/inicio/productos-recientes.php
@@ -55,7 +55,7 @@ $productos = ControladorProductos::ctrMostrarProductos($item, $valor, $orden);
 
             '.$productos[$i]["descripcion"].'
 
-            <span class="label label-warning pull-right">$'.$productos[$i]["precio_venta"].'</span>
+            <!--<span class="label label-warning pull-right">$'.$productos[$i]["precio_venta"].'</span>-->
 
           </a>
     
